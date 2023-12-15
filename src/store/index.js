01b46b4a44cacac3c89acb6f3be20dce20a29f76
Pedro Lopez Biedma <plo@novatec-gmbh.de>
2023-12-15 10:44:24 +0100

    Coding the movies slice of the store

--- src/store/index.js
+++ src/store/index.js
@@ -14,12 +14,28 @@ const songsSlice = createSlice({
     }
 })
 
+const moviesSlice = createSlice({
+    name: 'movie',
+    initialState: [],
+    reducers: {
+        addMovie(state, action){
+            state.push(action.payload)
+        },
+        removeMovie(state, action){
+            const index = state.indexOf(action.payload)
+            state.splice(index, 1)
+        }
+    }
+})
+
 const store = configureStore({
     reducer: {
-        songs: songsSlice.reducer
+        songs: songsSlice.reducer,
+        movies: moviesSlice.reducer
     }
 })
 
 console.log('State is -->', store.getState());
 export { store };
 export const { addSong, removeSong } = songsSlice.actions
+export const { addMovie, removeMovie } = moviesSlice.actions
